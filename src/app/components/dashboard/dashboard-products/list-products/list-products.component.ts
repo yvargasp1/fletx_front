cde2e3f63d8bea90a441ffb93e1c5daab5d85c15
yvargasp1 <yavargasp1@ucentral.edu.co>
2024-05-06 10:59:11 -0500

    List products

--- src/app/components/dashboard/dashboard-products/list-products/list-products.component.ts
+++ src/app/components/dashboard/dashboard-products/list-products/list-products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../../../services/category/category.service';
+import { ProductService } from '../../../../services/product/product.service';
+import { Product } from '../../../../models/Product.dto';
+import { Category } from '../../../../models/Category.dto';
 
 @Component({
   selector: 'app-list-products',
@@ -7,15 +10,30 @@ import { CategoryService } from '../../../../services/category/category.service'
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent implements OnInit {
-  constructor(private categoryService :CategoryService){
+  constructor(private categoryService :CategoryService, private productService:ProductService){
 
   }
+  listCategories : Category[]=[]
+  listProducts:Product[]=[]
   ngOnInit(): void {
     this.categoryService.getAll().subscribe({
       next :(value)=> {
-            console.log(value)
+            this.listCategories = value
+      },
+      error :(err)=> {
+          console.log(err)
       },
     })
+    this.productService.getProducts('price', null, null, null).subscribe({
+      next: (value) => {
+        this.listProducts = value;
+        console.log(this.listProducts)
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    
   }
 
   
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Product } from '../../models/Product.dto';
@@ -12,5 +12,28 @@ export class ProductService {
 
   public saveProduct(data: Product) {
     return this.http.post<Product>(`${this.URL}/products`, data);
+  }
+
+  public getProducts(
+    sort: string | null,
+    lte: number | null,
+    gte: number | null,
+    category: number | null
+  ) {
+    let params = new HttpParams();
+
+    if (sort) {
+      params = params.set('sort', sort);
+    }
+    if (lte) {
+      params = params.set('lte', lte);
+    }
+    if (gte) {
+      params = params.set('gte', gte);
+    }
+    if (category) {
+      params = params.set('category', category);
+    }
+    return this.http.get<any>(`${this.URL}/products`, { params });
   }
 }
